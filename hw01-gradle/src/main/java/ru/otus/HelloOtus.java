78fe0d55375a30e857496af5eaa708aa0c28837c
alesya <yalesya.id@gmail.com>
2025-01-19 12:56:36 +0300

    Исправление старых классов в связи с добавлением плагина SonarLint

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -3,6 +3,7 @@ package ru.otus;
 import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableSet;
 
+@SuppressWarnings({"java:S106"})
 public class HelloOtus {
     public static void main(String[] args) {
         ImmutableCollection<String> collection = ImmutableSet.of("one", "two", "three");
